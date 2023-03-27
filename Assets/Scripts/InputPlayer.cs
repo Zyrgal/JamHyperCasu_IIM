cc7dd9e629e8b4d7e33801088d9c8eb78eeb6f2e
Zyrgal <Valud64@hotmail.fr>
2023-03-27 15:34:53 +0200

    Move velocity

--- Assets/Scripts/InputPlayer.cs
+++ Assets/Scripts/InputPlayer.cs
@@ -7,14 +7,32 @@ public class InputPlayer : MonoBehaviour
     [SerializeField]
     float speed = 1f;
 
+    Rigidbody rb;
 
+    bool isDead = false;
     bool playerIsMoving = false;
+
+
     public bool PlayerIsMoving { get => playerIsMoving; }
     public bool IsDead { /*get => isDead;*/ set => isDead = value; }
 
-    bool isDead = false;
 
-    void Update()
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            rb.velocity = Vector3.zero;
+            Debug.Log("playerIsMoving = false");
+            playerIsMoving = false;
+        }
+    }
+
+    void FixedUpdate()
     {
         if (isDead)
         {
@@ -23,19 +41,19 @@ public class InputPlayer : MonoBehaviour
 
         if (Input.GetButton("Fire1"))
         {
+
+            rb.velocity = transform.forward * speed * Time.deltaTime;
+
             if (!playerIsMoving)
             {
                 Debug.Log("playerIsMoving = true");
                 playerIsMoving = true;
             }
+
+
             gameObject.transform.position += Vector3.forward * speed * Time.deltaTime;
         }
 
-        if (Input.GetButtonUp("Fire1"))
-        {
-            Debug.Log("playerIsMoving = false");
-            playerIsMoving = false;
-        }
     }
 
     public void CheckIfPlayerDie()
